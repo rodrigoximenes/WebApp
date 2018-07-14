8cb34928701da5c1aba5216826109042f382a54b
Rodrigo <Rodrigo>
2018-07-14 17:34:39 -0300

    Pagina

---
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,15 @@
+﻿console.log("ta procurando o que aqui");
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButon");
+button.addEventListener("click", function () {
+    console.log("Buying Item");
+})
+
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItem = productInfo.item[0].children;
+
+
