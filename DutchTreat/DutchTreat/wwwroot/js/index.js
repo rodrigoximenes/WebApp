3f9e55caf212dd3e2d4e2f9ff2df197b161a76fb
Rodrigo <Rodrigo>
2018-07-14 20:02:27 -0300

    jQuery

--- DutchTreat/DutchTreat/wwwroot/js/index.js
+++ DutchTreat/DutchTreat/wwwroot/js/index.js
@@ -1,15 +1,29 @@
-﻿console.log("ta procurando o que aqui");
+﻿$(document).ready(function () {
+    console.log("ta procurando o que aqui");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
 
-var button = document.getElementById("buyButon");
-button.addEventListener("click", function () {
-    console.log("Buying Item");
-})
+    var button = $("#buyButon");
+    button.on("click", function () {
+        console.log("Buying Item");
+    })
 
 
-var productInfo = document.getElementsByClassName("product-props");
-var listItem = productInfo.item[0].children;
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("Você clicou em " + $(this).text());
+    });
 
+
+
+    var $loginToogle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToogle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+
+
+});
 
