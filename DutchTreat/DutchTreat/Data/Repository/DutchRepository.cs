d4313a631d67a4c227b29e5e1c037c3b22bdbca9
Rodrigo <Rodrigo>
2018-07-21 13:15:54 -0300

    Logando repositorio

--- DutchTreat/DutchTreat/Data/Repository/DutchRepository.cs
+++ DutchTreat/DutchTreat/Data/Repository/DutchRepository.cs
@@ -1,22 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DutchTreat.Data.Context;
 using DutchTreat.Data.Entities;
+using Microsoft.Extensions.Logging;
 
 namespace DutchTreat.Data.Repository
 {
     public class DutchRepository : IDutchRepository
     {
         private readonly DutchContext _ctx;
+        private readonly ILogger<DutchRepository> _logger;
 
-        public DutchRepository(DutchContext ctx)
+        public DutchRepository(DutchContext ctx, ILogger<DutchRepository> logger)
         {
             _ctx = ctx;
+            _logger = logger;
         }
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _ctx.Products.OrderBy(p => p.Title).ToList();
+            try
+            {
+                _logger.LogInformation("Todos produtos chamados");
+                return _ctx.Products.OrderBy(p => p.Title).ToList();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to get all products: {e}");
+                return null;
+            }
         }
 
         public IEnumerable<Product> GetProductsByCategory(string category)
